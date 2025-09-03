5a45191213d75fe40880557c56e7dcee471ae1ce
KarlaMacedo <karlapaolamacedo@gmail.com>
2025-09-03 08:45:30 -0600

    Upload fetch data

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const API_URL = "https://api.escuelajs.co/api/v1";
+
+//Fetch products and categories
+async function fetchData(endpoint) {
+    try {
+        const response = await fetch(`${API_URL}/${endpoint}`);
+        if (!response.ok) {
+            throw new Error("Error fetching data");
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("There was a problem with the fetch data:", error);
+    }
+}
+
+async function loadProducts() {
+    try {
+        const products = await fetchData("products?limit=10&offset=0");
+        console.log("Productos:", products);
+        return products;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function loadCategories() {
+    try {
+        const categories = await fetchData("categories?limit=40");
+        console.log("Categorías:", categories);
+        return categories;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+loadProducts();
+loadCategories();
